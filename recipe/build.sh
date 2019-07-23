c9ba43e668ba27ae3db04163d9c9bd378b3d3e7d
Peter Williams <peter@newton.cx>
2019-07-23 01:59:19 -0400

    Fix build on macOS, due to esoteric linker arg interaction

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,14 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [ -n "$OSX_ARCH" ] ; then
     export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+
+    # The -dead_strip_dylibs option breaks g-ir-scanner in this package: the
+    # scanner uses the linker to find paths to dylibs, and it wants to find
+    # libpoppler.dylib, but with this option the linker strips the library
+    # from the test executable. The error message is "ERROR: can't resolve
+    # libraries to shared libraries: poppler".
+    export LDFLAGS="$(echo $LDFLAGS |sed -e "s/-Wl,-dead_strip_dylibs//g")"
+    export LDFLAGS_LD="$(echo $LDFLAGS_LD |sed -e "s/-dead_strip_dylibs//g")"
 else
     export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 fi
