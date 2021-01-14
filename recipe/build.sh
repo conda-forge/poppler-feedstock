86db8c5eb3ca49d3e6f8cd9c27d6c39e084208d7
Uwe L. Korn <uwe.korn@quantco.com>
2021-01-14 11:08:12 +0100

    Cross-compile support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [ -n "$OSX_ARCH" ] ; then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+export EXTRA_CMAKE_ARGS="-GNinja -DCMAKE_INSTALL_LIBDIR=lib -DENABLE_UNSTABLE_API_ABI_HEADERS=ON -DENABLE_LIBCURL=ON -DENABLE_LIBOPENJPEG=openjpeg2"
 
+if [ -n "$OSX_ARCH" ] ; then
     # The -dead_strip_dylibs option breaks g-ir-scanner in this package: the
     # scanner uses the linker to find paths to dylibs, and it wants to find
     # libpoppler.dylib, but with this option the linker strips the library
@@ -12,20 +12,49 @@ if [ -n "$OSX_ARCH" ] ; then
     # libraries to shared libraries: poppler".
     export LDFLAGS="$(echo $LDFLAGS |sed -e "s/-Wl,-dead_strip_dylibs//g")"
     export LDFLAGS_LD="$(echo $LDFLAGS_LD |sed -e "s/-dead_strip_dylibs//g")"
-else
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+fi
+
+if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
+    unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+    (
+        mkdir -p native-build
+        pushd native-build
+
+        export CC=$CC_FOR_BUILD
+        export CXX=$CC_FOR_BUILD
+        export AR=$($CC_FOR_BUILD -print-prog-name=ar)
+        export NM=$($CC_FOR_BUILD -print-prog-name=nm)
+        export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+        export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+        # Unset them as we're ok with builds that are either slow or non-portable
+        unset CFLAGS
+        unset CPPFLAGS
+        unset CXXFLAGS
+
+	cmake ${EXTRA_CMAKE_ARGS} \
+	    -DCMAKE_PREFIX_PATH=$BUILD_PREFIX \
+	    -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX \
+	    $SRC_DIR
+        # This script would generate the functions.txt and dump.xml and save them
+        # This is loaded in the native build. We assume that the functions exported
+        # by glib are the same for the native and cross builds
+        export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
+        ninja -j$CPU_COUNT -v
+        ninja install
+        popd
+    )
+    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
 fi
 
 mkdir build && cd build
 
-cmake ${CMAKE_ARGS} \
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig"
+
+cmake ${CMAKE_ARGS} ${EXTRA_CMAKE_ARGS} \
     -GNinja \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
-    -DENABLE_LIBCURL=ON \
-    -DENABLE_LIBOPENJPEG=openjpeg2 \
     $SRC_DIR
 
 ninja
